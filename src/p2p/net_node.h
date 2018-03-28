cd28ef518fe94d173d16094a7e3ed3ab793256b6
Italocoin Project <12392580+italocoin-project@users.noreply.github.com>
2018-03-28 17:05:43 +0200

    Update net_node.h

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -127,10 +127,8 @@ namespace nodetool
     virtual std::map<std::string, time_t> get_blocked_hosts() { CRITICAL_REGION_LOCAL(m_blocked_hosts_lock); return m_blocked_hosts; }
   private:
     const std::vector<std::string> m_seed_nodes_list =
-    { "seeds.italocoinseeds.se"
-    , "seeds.italocoinseeds.ae.org"
-    , "seeds.italocoinseeds.ch"
-    , "seeds.italocoinseeds.li"
+    { "seeds.italocoin.com"
+    , "seedstwo.italocoin.com"
     };
 
     bool islimitup=false;
