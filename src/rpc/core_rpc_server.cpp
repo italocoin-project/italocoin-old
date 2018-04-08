0d86c41c72ddd4340fc24283b7a0a7823daef436
Italocoin Project <12392580+italocoin-project@users.noreply.github.com>
2018-04-08 13:42:28 +0200

    Update core_rpc_server.cpp

--- src/rpc/core_rpc_server.cpp
+++ src/rpc/core_rpc_server.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 The Monero And Italocoin Project
+// Copyright (c) 2014-2018, The Italocoin And Monero Project
 // 
 // All rights reserved.
 // 
@@ -2117,7 +2117,7 @@ namespace cryptonote
     , "Port for RPC server"
     , std::to_string(config::RPC_DEFAULT_PORT)
     , {{ &cryptonote::arg_testnet_on, &cryptonote::arg_stagenet_on }}
-    , [](std::array<bool, 2> testnet_stagenet, bool defaulted, std::string val) {
+    , [](std::array<bool, 2> testnet_stagenet, bool defaulted, std::string val)->std::string {
         if (testnet_stagenet[0] && defaulted)
           return std::to_string(config::testnet::RPC_DEFAULT_PORT);
         else if (testnet_stagenet[1] && defaulted)
